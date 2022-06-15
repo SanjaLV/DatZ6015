bb3a5b455c9f82ba65a686ebfd00b8edc128b146
Aleksandrs Zajakins <ghostsanja@gmail.com>
2022-06-15 03:03:52 +0300

    HW2: enc/dec

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
 
 #include <string>
 #include <vector>
@@ -84,7 +85,7 @@ void create_certificate(const std::string& settings_file, const std::string& pri
     printf("DONE\n");
 }
 
-void verify_certificate(const std::string& cert) {
+X509* read_cert_from_file(const std::string& cert) {
     FILE * f = fopen(cert.c_str(), "rb");
 
     if (f == nullptr) {
@@ -94,6 +95,14 @@ void verify_certificate(const std::string& cert) {
 
     X509 * x509 = X509_new();
     PEM_read_X509(f, &x509, nullptr, nullptr);
+    
+    fclose(f);
+
+    return x509;
+}
+
+void verify_certificate(const std::string& cert) {
+    X509* x509 = read_cert_from_file(cert);
 
     const int BUFFER_SIZE = 256;
     static char buffer[BUFFER_SIZE], buffer2[BUFFER_SIZE];
@@ -125,14 +134,118 @@ void verify_certificate(const std::string& cert) {
     }
 }
 
+void encrypt(const std::string& ifile, const std::string& ofile, const std::string& cert_file) {
+    X509* x509 = read_cert_from_file(cert_file);
+    
+    EVP_PKEY *pkey= X509_get_pubkey(x509);
+    RSA * rsa = EVP_PKEY_get1_RSA(pkey);
+
+    FILE* fin  = fopen(ifile.c_str(), "rb");
+    FILE* fout = fopen(ofile.c_str(), "wb");
+
+    if (fin == nullptr) {
+        fprintf(stderr, "Cannot open file '%s'\n", ifile.c_str());
+        exit(1);
+    }
+    if (fout == nullptr) {
+        fprintf(stderr, "Cannot create file '%s'\n", ifile.c_str());
+        exit(1);
+    }
+
+    const int intput_block_size = RSA_size(rsa) - 42;
+    const int output_block_size = RSA_size(rsa);
+
+    unsigned char * input_buf  = static_cast<unsigned char *>(malloc(intput_block_size));
+    unsigned char * output_buf = static_cast<unsigned char *>(malloc(output_block_size));
+
+    while (true) {
+        int read = fread(input_buf, 1, intput_block_size, fin);
+        if (read == 0) break;
+
+        int enc_size = RSA_public_encrypt(read,      // input_size
+                                          input_buf,
+                                          output_buf,
+                                          rsa,
+                                          RSA_PKCS1_OAEP_PADDING);
+
+        if (enc_size > 0) {
+            fwrite(output_buf, 1, enc_size, fout);
+        }
+
+    }
+
+    fclose(fin);
+    fclose(fout);
+}
+
+EVP_PKEY* read_private_key_from_file(const std::string& file) {
+    FILE * f = fopen(file.c_str(), "rb");
+
+    if (f == nullptr) {
+        fprintf(stderr, "Cannot open file '%s'\n", file.c_str());
+        exit(1);
+    }
+
+    EVP_PKEY* key = EVP_PKEY_new();
+    
+    PEM_read_PrivateKey(f, &key, nullptr, nullptr);
+    assert(key != nullptr);
+
+    fclose(f);
+    
+    return key;
+}
+
+void decrypt(const std::string& ifile, const std::string& ofile, const std::string& private_key) {
+
+    FILE* fin  = fopen(ifile.c_str(), "rb");
+    FILE* fout = fopen(ofile.c_str(), "wb");
+
+    if (fin == nullptr) {
+        fprintf(stderr, "Cannot open file '%s'\n", ifile.c_str());
+        exit(1);
+    }
+    if (fout == nullptr) {
+        fprintf(stderr, "Cannot create file '%s'\n", ifile.c_str());
+        exit(1);
+    } 
+
+    EVP_PKEY* pkey = read_private_key_from_file(private_key);
+    RSA * rsa = EVP_PKEY_get1_RSA(pkey);
+
+    const int block_size = RSA_size(rsa);
+    unsigned char * input_buf  = static_cast<unsigned char *>(malloc(block_size));
+    unsigned char * output_buf = static_cast<unsigned char *>(malloc(block_size));
+
+    while (true) {
+        int read = fread(input_buf, 1, block_size, fin);
+        if (read == 0) break;
+
+        int enc_size = RSA_private_decrypt(read,
+                                          input_buf,
+                                          output_buf,
+                                          rsa,
+                                          RSA_PKCS1_OAEP_PADDING);
+
+        if (enc_size > 0) {
+            fwrite(output_buf, 1, enc_size, fout);
+        }
+
+    }
+
+
+    fclose(fin);
+    fclose(fout);
+}
+
 void help(const std::string& progname) {
     printf("Usage %s --mode [...args]\n", progname.c_str());
     printf("Where mode is one of the following:\n");
     printf("\tcreate settings_file key.pem\n");
     printf("\tverify cert.pem\n");
-    printf("\tencrypt file key.pem\n");
-    printf("\tdecrypt file key.pem\n");
-    printf("\thelp - display this message\n");
+    printf("\tencrypt cert.pem input_file output_file\n");
+    printf("\tdecrypt private_key.pem input_file output_file\n");
+    printf("\thelp - display this message\n"); 
     exit(0);
 }
 
@@ -149,8 +262,14 @@ int modern_main(const std::vector<std::string>& args) {
         verify_certificate(args[2]);
     }
     else if (args[1] == "--encrypt") {
+        if (args.size() != 5) help(args[0]);
+
+        encrypt(args[3], args[4], args[2]);
     }
     else if (args[1] == "--decrypt") {
+        if (args.size() != 5) help(args[0]);
+
+        decrypt(args[3], args[4], args[2]);
     }
     else {
         help(args[0]);
